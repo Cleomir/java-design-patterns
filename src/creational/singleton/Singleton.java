190fbc15c454b8985fa532fcf840a429f22d9bc9
Cleomir Machado Gomes <cleomir.mg@hotmail.com>
2024-02-09 22:10:47 -0300

    Add singleton pattern

---
+++ src/creational/singleton/Singleton.java
@@ -0,0 +1,20 @@
+package creational.singleton;
+
+public class Singleton {
+
+  private static Singleton instance;
+
+  private Singleton() {}
+
+  public static synchronized Singleton getInstance() {
+    if (instance == null) {
+      instance = new Singleton();
+    }
+
+    return instance;
+  }
+
+  public void doSomething() {
+    System.out.println("Doing something...");
+  }
+}
